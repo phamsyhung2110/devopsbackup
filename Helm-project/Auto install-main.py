2afe9e53442c5130a8babe9ef786e276ce8d39ec
Hung <phamsyhung2110@gmail.com>
2022-05-07 22:35:30 +0700

    Update multiple input file 7/5/2022

--- Helm-project/Auto install-main.py
+++ Helm-project/Auto install-main.py
@@ -1,4 +1,5 @@
 from base64 import encode
+from genericpath import isfile
 import os,shutil,glob,sys,re
 from pydoc import describe
 import subprocess
@@ -7,10 +8,12 @@ from unicodedata import name
 from jinja2 import Template
 #
 action = sys.argv[1]
-app_name = sys.argv[2]
-env = sys.argv[3]
-mod_nginx = sys.argv[4]
-pattern = app_name
+release_name = sys.argv[2]
+chart_dir = sys.argv[3]
+env = sys.argv[4]
+mod_nginx = sys.argv[5]
+pattern = "%s*"%chart_dir
+print(pattern)
 project_dir = "./Helm-Project/"
 def get_root_app():
     global pattern,project_dir
@@ -19,20 +22,32 @@ def get_root_app():
             path_app = os.path.join(root,dirname) # Path from specified directory
             root_app = os.path.abspath(path_app)  # Path from root directory
             return root_app
+# def get_input_file():
+#     global chart_dir, env, project_dir
+#     input_dir =project_dir + "Input/%s/%s/"%(env,chart_dir)
+#     # pattern = "input-%s-%s*.yaml"%(env,chart_dir)
+#     for file in glob.glob(input_dir + "*.yaml"):
+#         # print("Input file directory: " + os.path.abspath(file))
+#         file_locate = str(os.path.abspath(file))
+#         return file_locate
 def get_input_file():
-    global app_name, env, project_dir
-    input_dir =project_dir + "Input/%s/%s/"%(env,app_name)
-    # pattern = "input-%s-%s*.yaml"%(env,app_name)
-    for file in glob.glob(input_dir + "*.yaml"):
-        # print("Input file directory: " + os.path.abspath(file))
-        file_locate = str(os.path.abspath(file))
-        return file_locate
+    global chart_dir, env, project_dir
+    input_dir =project_dir + "Input/"
+    # pattern = "input-%s-%s*.yaml"%(env,chart_dir)
+    for root,dirs,files in os.walk(input_dir + "/%s/%s/"%(chart_dir,env)):
+        for file in files:
+            print(file)
+            if file.endswith("yaml"):
+                file_locate = str(os.path.join(root,file))
+                print(file_locate)
+            return file_locate
+print("File locate: " + str(get_input_file()))
 def get_custom_value(value_dir,run_dir,conf_dir):
-    global app_name, env, project_dir
+    global chart_dir, env, project_dir
     path_value_file = ""
     path_conf_file = ""
     path_run_file = ""
-    # value_dir = project_dir +  "Custom-value-template/%s/%s/"%(env,app_name)
+    # value_dir = project_dir +  "Custom-value-template/%s/%s/"%(env,chart_dir)
     for file in glob.glob(value_dir + "*.j2"):
         path_value_file = os.path.abspath(file)
     # for conf_file in glob.glob(value_dir + "conf/*.conf.j2"):
@@ -45,9 +60,9 @@ def get_custom_value(value_dir,run_dir,conf_dir):
 root_app = str(get_root_app()) + "/"
 input_file = str(get_input_file())
 ### Template file directory ###
-value_dir = project_dir +  "Custom-value-template/%s/%s/"%(env,app_name)
-run_dir = project_dir +  "Run-template/%s/%s/"%(env,app_name)
-conf_dir = project_dir +  "Config-template/%s/%s/"%(env,app_name)
+value_dir = project_dir +  "Custom-value-template/%s/%s/"%(env,chart_dir)
+run_dir = project_dir +  "Run-template/%s/%s/"%(env,chart_dir)
+conf_dir = project_dir +  "Config-template/%s/%s/"%(env,chart_dir)
 ###
 value_file_tem = str(get_custom_value(value_dir,run_dir,conf_dir)[0])
 run_file_tem = str(get_custom_value(value_dir,run_dir,conf_dir)[2])
@@ -55,13 +70,38 @@ run_file_tem = str(get_custom_value(value_dir,run_dir,conf_dir)[2])
 print("App:  "+ root_app)
 print("Input file: "+ input_file)
 print("Value file:  " + value_file_tem)
-def get_input_open():
-        with open(input_file, 'r', encoding='UTF-8') as file:
-            return file.read()
+# def get_input_open():
+#         data = {}
+#         for per_input_file in input_file:
+#             with open(per_input_file, 'r', encoding='UTF-8') as file:
+#                 # return file.read()
+#                 yaml_data_input = yaml.safe_load(file)
+#                 for key,value in yaml_data_input.items():
+#                     data.update({key:value})
+#                     data_input = yaml.dump(data).replace("null","")
+#                     print(data_input)
+#                     return data_input
+def get_input_file():
+    global chart_dir, env, project_dir
+    input_dir =project_dir + "Input/"
+    data = {}
+    for root,dir,files in os.walk(input_dir + "/%s/%s/"%(chart_dir,env)):
+        for file in files:
+            if file.endswith("yaml"):
+                file_locate = str(os.path.join(root,file))
+                a = open(file_locate, "r")   
+                input = yaml.safe_load(a)
+                for key,value in input.items():
+                    data.update({key:value})
+    data_input = yaml.dump(data).replace("null", "")
+    print(yaml.dump(data).replace("null", ""))
+    return data_input
+yaml_input_dumped  = get_input_file()
 
 def change_values_file(render_to,tem_file):
-    global value_file_tem, input_file, root_app,run_file_tem
-    # render_to = project_dir + "Custom-value-complete/value/%s/%s/"%(app_name,env) + os.path.basename(value_file_tem)
+    print("2")
+    global value_file_tem, input_file, root_app,run_file_tem, yaml_input_dumped
+    # render_to = project_dir + "Custom-value-complete/value/%s/%s/"%(chart_dir,env) + os.path.basename(value_file_tem)
     name_split = render_to.split(".j2")[0]
     # value_file_tem_yaml =root_app + name_split 
     def save_values(out_put):
@@ -70,25 +110,22 @@ def change_values_file(render_to,tem_file):
             print("New file generated " + name_split)    
     def get_template_values():
         with open(tem_file , 'r', encoding='UTF-8') as file:
-            return file.read()  
-    # def get_template_run():
-    #     with open(value_file_tem , 'r', encoding='UTF-8') as file:
-    #         return file.read()  
-    input = yaml.safe_load(get_input_open())
+            return file.read()   
+    data_input_final = yaml.safe_load(yaml_input_dumped)
     template_input = get_template_values()
     template_values = Template(template_input) 
-    out_put = template_values.render(**input)
+    out_put = template_values.render(**data_input_final)
     save_values(out_put)
     return name_split
 
 if mod_nginx=="--mod-nginx":
     # conf_file = str(get_custom_value(render_config)[1])
     conf_file_tem =  str(get_custom_value(value_dir,run_dir,conf_dir)[1])
-    helm_chart_folder = project_dir + "Helm-app/%s/config/"%app_name 
+    helm_chart_folder = project_dir + "Helm-app/%s/config/"%chart_dir 
     print("Config file: "+ conf_file_tem)
     def change_nginx(render_config):
-        global conf_file_tem, input_file, root_app, project_dir, env
-        # render_config = project_dir + "Config/%s/%s/"%(env,app_name) + os.path.basename(conf_file_tem)
+        global conf_file_tem, input_file, root_app, project_dir, env, yaml_input_dumped
+        # render_config = project_dir + "Config/%s/%s/"%(env,chart_dir) + os.path.basename(conf_file_tem)
         name_split = render_config.split(".j2")[0]
         def save_nginx(out_put):                                  ## Save output rendered
             with open(name_split , 'w', encoding='UTF-8') as file:
@@ -96,10 +133,10 @@ if mod_nginx=="--mod-nginx":
         def get_template_nginx():
             with open(conf_file_tem, 'r', encoding='UTF-8') as file:
                 return file.read()
-        input = yaml.safe_load(get_input_open())
+        data_input_final = yaml.safe_load(yaml_input_dumped)
         nginx_template = get_template_nginx()
         template = Template(nginx_template)
-        out_put = template.render(**input)
+        out_put = template.render(**data_input_final)
         save_nginx(out_put)
         shutil.copy2(name_split, helm_chart_folder)
         return name_split
@@ -107,16 +144,16 @@ if mod_nginx=="--mod-nginx":
 else: print("No nginx config") 
 
 
-# run_dir = project_dir +  "Run-template/%s/%s/"%(env,app_name)
+# run_dir = project_dir +  "Run-template/%s/%s/"%(env,chart_dir)
 ## Template file
 value_file_tem = str(get_custom_value(value_dir,run_dir,conf_dir)[0])
 run_file_tem = str(get_custom_value(value_dir,run_dir,conf_dir)[2])  
 ## Conf_file_tem define in change__nginx()
 ## Render to
-render_value = project_dir + "Custom-value-complete/value/%s/%s/"%(env,app_name) + os.path.basename(value_file_tem)
-render_run = project_dir + "Custom-value-complete/run/%s/%s/"%(env,app_name) + os.path.basename(run_file_tem)
+render_value = project_dir + "Custom-value-complete/value/%s/%s/"%(env,chart_dir) + os.path.basename(value_file_tem)
+render_run = project_dir + "Custom-value-complete/run/%s/%s/"%(env,chart_dir) + os.path.basename(run_file_tem)
 if mod_nginx == "--mod-nginx":
-    render_config = project_dir + "Custom-value-complete/config/%s/%s/"%(env,app_name) + os.path.basename(conf_file_tem)
+    render_config = project_dir + "Custom-value-complete/config/%s/%s/"%(env,chart_dir) + os.path.basename(conf_file_tem)
     conf_file = change_nginx(render_config)
 else:
     print(" ")
@@ -124,22 +161,22 @@ else:
 value_file = change_values_file(render_value,value_file_tem)
 run_file = change_values_file(render_run,run_file_tem)
 
-
 def install():
-    global env, action, app_name, root_app, value_file, run_file
-    # os.chdir(root_app)   
-    run_dir = project_dir +  "Custom-value-template/%s/%s/"%(env,app_name) + "run"
+    print("3")
+    global env, action, chart_dir, root_app, value_file, run_file, release_name
+    # os.chdir(root_app)   s
+    run_dir = project_dir +  "Custom-value-template/%s/%s/"%(env,chart_dir) + "run"
     print("Executing command: kubectl apply -f " + run_file)
     time.sleep(0.25)
-    subprocess.call("kubectl apply -f " + run_file)
+    # subprocess.call("kubectl apply -f " + run_file)
     
     ## Action
-    print("Executing command: helm " + action + " " + app_name + " "  + project_dir + "Helm-app/%s/ -f "%app_name + value_file )
+    print("Executing command: helm " + action + " " + release_name + " "  + project_dir + "Helm-app/%s/ -f "%chart_dir + value_file )
     time.sleep(0.5)
-    subprocess.call("helm " + action + " " + app_name + " " + project_dir + "Helm-app/%s/ -f "%app_name + value_file)
+    # subprocess.call("helm " + action + " " + chart_dir + " " + project_dir + "Helm-app/%s/ -f "%chart_dir + value_file)
 def uninstall():
-        print("Executing command: helm uninstall %s"%app_name )
-        subprocess.call("helm uninstall %s"%app_name)
+        print("Executing command: helm uninstall %s"%release_name )
+        # subprocess.call("helm uninstall %s"%chart_dir)
 if action == "uninstall":
     uninstall()
 else:
